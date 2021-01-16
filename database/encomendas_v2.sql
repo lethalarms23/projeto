6c00ffbe99affa24624a0cef6bf34bed31778c84
Marcelo Nogueira <66036697+lethalarms23@users.noreply.github.com>
2021-01-16 14:49:41 +0000

    avaliacao M17

--- database/encomendas_v2.sql
+++ database/encomendas_v2.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 14-Jan-2021 às 15:39
--- Versão do servidor: 10.1.38-MariaDB
--- versão do PHP: 7.3.2
+-- Tempo de geração: 16-Jan-2021 às 15:48
+-- Versão do servidor: 10.4.17-MariaDB
+-- versão do PHP: 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `encomendas_v2`
+-- Banco de dados: `encomendas_v2`
 --
 
 -- --------------------------------------------------------
@@ -80,9 +79,9 @@ CREATE TABLE `encomendas_produtos` (
   `id_enc_prod` int(11) NOT NULL,
   `id_produto` int(11) NOT NULL,
   `id_encomenda` int(11) NOT NULL,
-  `quantidade` int(11) DEFAULT '0',
-  `preco` double DEFAULT '0',
-  `desconto` double DEFAULT '0',
+  `quantidade` int(11) DEFAULT 0,
+  `preco` double DEFAULT 0,
+  `desconto` double DEFAULT 0,
   `obervacoes` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL
@@ -137,7 +136,7 @@ CREATE TABLE `fornecedores_produtos` (
   `id_for_prod` int(11) NOT NULL,
   `id_fornecedor` int(11) DEFAULT NULL,
   `id_produto` int(11) DEFAULT NULL,
-  `preco` double DEFAULT '0',
+  `preco` double DEFAULT 0,
   `observacoes` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL
@@ -152,6 +151,25 @@ INSERT INTO `fornecedores_produtos` (`id_for_prod`, `id_fornecedor`, `id_produto
 (2, 1, 2, 150, NULL, '2020-11-05 13:02:22', '2020-11-05 13:02:22'),
 (3, 2, 3, 100, NULL, '2020-11-05 13:20:52', '2020-11-05 13:20:52'),
 (4, 2, 4, 150, NULL, '2020-11-05 13:21:00', '2020-11-05 13:21:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `likes`
+--
+
+CREATE TABLE `likes` (
+  `id_user` int(11) NOT NULL,
+  `id_produto` int(11) NOT NULL,
+  `id_like` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `likes`
+--
+
+INSERT INTO `likes` (`id_user`, `id_produto`, `id_like`) VALUES
+(1, 4, 7);
 
 -- --------------------------------------------------------
 
@@ -174,8 +192,9 @@ CREATE TABLE `migrations` (
 CREATE TABLE `produtos` (
   `id_produto` int(11) NOT NULL,
   `designacao` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `stock` int(9) DEFAULT '0',
-  `preco` double DEFAULT '0',
+  `stock` int(9) DEFAULT 0,
+  `preco` double DEFAULT 0,
+  `imagem` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `id_categoria` int(11) DEFAULT NULL,
   `observacoes` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
@@ -186,10 +205,10 @@ CREATE TABLE `produtos` (
 -- Extraindo dados da tabela `produtos`
 --
 
-INSERT INTO `produtos` (`id_produto`, `designacao`, `stock`, `preco`, `id_categoria`, `observacoes`, `updated_at`, `created_at`) VALUES
-(1, 'TV LG', 100, 150, 2, NULL, '2020-12-02 00:00:00', '2020-12-02 00:00:00'),
-(2, 'TV SONY', 200, 50, 2, NULL, '2020-12-25 00:00:00', '2020-12-31 00:00:00'),
-(4, 'Apple iPhone', 200, 1000, 5, NULL, '2020-12-02 00:00:00', '2020-12-31 00:00:00');
+INSERT INTO `produtos` (`id_produto`, `designacao`, `stock`, `preco`, `imagem`, `id_categoria`, `observacoes`, `updated_at`, `created_at`) VALUES
+(1, 'TV LG', 100, 150, '', 2, NULL, '2020-12-02 00:00:00', '2020-12-02 00:00:00'),
+(2, 'TV SONY', 200, 50, '', 2, NULL, '2020-12-25 00:00:00', '2020-12-31 00:00:00'),
+(4, 'Apple iPhone', 200, 1000, '', 5, 'aa', '2021-01-16 13:56:10', '2020-12-31 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -200,6 +219,7 @@ INSERT INTO `produtos` (`id_produto`, `designacao`, `stock`, `preco`, `id_catego
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tipo_user` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'normal' COMMENT 'admin ou normal',
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -212,8 +232,10 @@ CREATE TABLE `users` (
 -- Extraindo dados da tabela `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Marcelo', 'hiddankill@gmail.com', NULL, '$2y$10$wfycg.V.rOPEysknw51BWuqxz9VemaFDsioZWCQJrOZHmoklbkaLy', 'rbReklWd3BPutNVyqLhZSztzcBKVLli3kJbPjeqNYtIDrXUrhNpYwn9xrwwz', '2021-01-14 14:01:22', '2021-01-14 14:01:22');
+INSERT INTO `users` (`id`, `name`, `tipo_user`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Marcelo', 'admin', 'hiddankill@gmail.com', NULL, '$2y$10$wfycg.V.rOPEysknw51BWuqxz9VemaFDsioZWCQJrOZHmoklbkaLy', 'EExCiKQzMoH5xrk3harEkLdgx4YDagBI32dSYzfa7lqisG5IeVpB1jRhoZtl', '2021-01-14 14:01:22', '2021-01-14 14:01:22'),
+(2, 'Marcelo Nogueira', 'normal', 'hiddankill@protonmail.com', NULL, '$2y$10$mUAPzosI/FRrCn2y0LuPp.48SDL4ys.YfgbOgDMu1XQhyxWQYDJMi', 'fkBbDyxnagoGN4aQmz8bIGDJuFk6eC48iRBqopGCIEZQNgdFlpaQioBznRuD', '2021-01-16 11:23:00', '2021-01-16 11:23:00'),
+(5, 'Lethal Arms', 'normal', 'hiddan2020@gmail.com', NULL, '$2y$10$MdZ.7561ZxynZICYVQ6hKOfz.2lXaiRmdDBrW.3baXT6zwEotjH4O', NULL, '2021-01-16 14:38:12', '2021-01-16 14:38:12');
 
 -- --------------------------------------------------------
 
@@ -242,111 +264,123 @@ INSERT INTO `vendedores` (`id_vendedor`, `nome`, `especialidade`, `email`, `upda
 (5, 'Tiago Machado', 'Informática', 'tmachado@gmail.com', '2020-11-05 00:00:00', '2020-11-05 00:00:00');
 
 --
--- Indexes for dumped tables
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `categorias`
+-- Índices para tabela `categorias`
 --
 ALTER TABLE `categorias`
   ADD PRIMARY KEY (`id_categoria`);
 
 --
--- Indexes for table `encomendas`
+-- Índices para tabela `encomendas`
 --
 ALTER TABLE `encomendas`
   ADD PRIMARY KEY (`id_encomenda`);
 
 --
--- Indexes for table `encomendas_produtos`
+-- Índices para tabela `encomendas_produtos`
 --
 ALTER TABLE `encomendas_produtos`
   ADD PRIMARY KEY (`id_enc_prod`);
 
 --
--- Indexes for table `fornecedores`
+-- Índices para tabela `fornecedores`
 --
 ALTER TABLE `fornecedores`
   ADD PRIMARY KEY (`id_fornecedor`);
 
 --
--- Indexes for table `fornecedores_produtos`
+-- Índices para tabela `fornecedores_produtos`
 --
 ALTER TABLE `fornecedores_produtos`
   ADD PRIMARY KEY (`id_for_prod`);
 
 --
--- Indexes for table `migrations`
+-- Índices para tabela `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id_like`);
+
+--
+-- Índices para tabela `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `produtos`
+-- Índices para tabela `produtos`
 --
 ALTER TABLE `produtos`
   ADD PRIMARY KEY (`id_produto`);
 
 --
--- Indexes for table `users`
+-- Índices para tabela `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `vendedores`
+-- Índices para tabela `vendedores`
 --
 ALTER TABLE `vendedores`
   ADD PRIMARY KEY (`id_vendedor`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT for table `categorias`
+-- AUTO_INCREMENT de tabela `categorias`
 --
 ALTER TABLE `categorias`
   MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `encomendas`
+-- AUTO_INCREMENT de tabela `encomendas`
 --
 ALTER TABLE `encomendas`
   MODIFY `id_encomenda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `encomendas_produtos`
+-- AUTO_INCREMENT de tabela `encomendas_produtos`
 --
 ALTER TABLE `encomendas_produtos`
   MODIFY `id_enc_prod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `fornecedores`
+-- AUTO_INCREMENT de tabela `fornecedores`
 --
 ALTER TABLE `fornecedores`
   MODIFY `id_fornecedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `migrations`
+-- AUTO_INCREMENT de tabela `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id_like` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de tabela `migrations`
 --
 ALTER TABLE `migrations`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `produtos`
+-- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `vendedores`
+-- AUTO_INCREMENT de tabela `vendedores`
 --
 ALTER TABLE `vendedores`
   MODIFY `id_vendedor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
