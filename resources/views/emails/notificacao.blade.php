b2bec6d8815bd098b6768d5c1679d620fc9d8300
Marcelo Nogueira <66036697+lethalarms23@users.noreply.github.com>
2021-01-16 14:48:03 +0000

    avaliacao M17

--- resources/views/emails/notificacao.blade.php
+++ resources/views/emails/notificacao.blade.php
@@ -1,8 +1,36 @@
-@extends('layout')
-@section('header')
-Usuário
-@endsection
-@section('conteudo')
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/flag.min.css')}}">
+
+    <script src="{{asset('js/jquery-3.5.1.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/all.min.js')}}"></script>
+    <script src="{{asset('js/mdb.js')}}"></script>
+    <style>
+    body{
+        background-color: #2b2b2b;
+        color: #ffffff;
+    }
+    a{
+        color: #ffffff;
+    }
+    table,tr,td,th{
+      color: #ffffff;
+      text-align: center;
+    }
+    th {
+      text-decoration: underline;
+    }
+    .floatLeft { width: 50%; float: left; }
+    .floatRight {width: 50%; float: right; }
+    .container { overflow: hidden; }
+    </style>
+</head>
+<body>
 <div class="container">
 <div class="floatLeft">
 <table class="table table-dark table-striped">
@@ -21,4 +49,5 @@ Usuário
 </table>
 </div>
 <div class="floatRight">
-@endsection
+</body>
+</html>
